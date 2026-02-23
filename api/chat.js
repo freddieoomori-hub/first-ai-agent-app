41fa49ecb10c7e9193e2986dcd4b045a57d3aca8
freddieoomori-hub <freddieoomori@gmail.com>
2026-02-23 12:47:00 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -18,13 +18,6 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Message is required" });
     }
 
-// ---
-//const message2 = "天気：" + message;
-// ---
-
-
-    
-
     const client = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY
     });
