83905faf13af352521a8d594d521f076dd65d718
freddieoomori-hub <freddieoomori@gmail.com>
2026-02-23 18:46:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ async function send() {
   //
 //  const input = document.getElementById("input").value;
 //
-  const input = "「" + document.getElementById("input").value + "」　を短歌や俳句の上の句と解釈して、それに対応する下の句のみを答えて。その他のテキストは一切要らないから。了解！とかも要らないから。解説も要らないから。";
+  const input = "「" + document.getElementById("input").value + "」　を短歌や俳句の上の句と解釈して、それに対応する下の句のみを答えて。有名な短歌や俳句がある場合は、そのまま下の句を答えても良い。その他のテキストは一切要らないから。了解！とかも要らないから。解説も要らないから。";
 
   const res = await fetch("/api/chat", {
     method: "POST",
@@ -15,4 +15,5 @@ async function send() {
   document.getElementById("output").textContent = data.reply;
 
 }
+
 
